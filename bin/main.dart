bf3c008d8a04ca6183e5007402226234e65fd33a
mcclayac <anthony.mcclay@me.com>
2020-08-07 15:19:19 -0400

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,12 @@
+void main(List<String> arguments) {
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print('Dart Quick Start Chapter 8');
+  print('8. Multithreaded Programming Using Future and Callable Classes');
+  // https://learning.oreilly.com/library/view/quick-start-guide/9781484255629/A490744_1_En_8_Chapter.html
+  
+}
